1efab0e87fb6adfea7027abdeed01bec9ec1b919
AfifaArshad718 <afifaarshad718@gmail.com>
2024-11-13 19:44:02 +0500

    Create liftoff.py

---
+++ sesson assignments/liftoff.py
@@ -0,0 +1,11 @@
+def main():
+    # Use a for loop to count down from 10 to 1
+    for i in range(10, 0, -1):
+        print(i, end=" ")  # Print each number followed by a space
+    
+    # Print Liftoff! after the countdown
+    print("Liftoff!")
+
+# This ensures the main function runs when the script is executed
+if __name__ == '__main__':
+    main()
