507121eb332f9f09310ea6ce2519fba9a457e1ad
AfifaArshad718 <afifaarshad718@gmail.com>
2024-11-13 19:54:08 +0500

    Create handout.py

---
+++ sesson assignments/advanced/handout.py
@@ -0,0 +1,36 @@
+Welcome to the High-Low Game!
+--------------------------------
+
+Round 1
+Your number is 8
+Do you think your number is higher or lower than the computer's?: lower
+You were right! The computer's number was 35
+Your score is now 1
+
+Round 2
+Your number is 88
+Do you think your number is higher or lower than the computer's?: higher
+Aww, that's incorrect. The computer's number was 100
+Your score is now 1
+
+Round 3
+Your number is 63
+Do you think your number is higher or lower than the computer's?: higher
+You were right! The computer's number was 5
+Your score is now 2
+
+Round 4
+Your number is 57
+Do you think your number is higher or lower than the computer's?: lower
+Aww, that's incorrect. The computer's number was 57
+Your score is now 2
+
+Round 5
+Your number is 22
+Do you think your number is higher or lower than the computer's?: lower
+Aww, that's incorrect. The computer's number was 1
+Your score is now 2
+
+Thanks for playing!
+Your final score is 2
+Good job, you played really well!
